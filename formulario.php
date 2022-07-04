d3ef989d6ea0adfff653c594980230f918affe02
DouglasOliveira <douglasoliveiradaluz10@gmail.com>
2022-07-04 09:43:24 -0300

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,21 @@
+<?php
+include_once 'index.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <input name="1" type="text">
+        <input name="2" type="text">
+        <input name="3" type="text">
+        <input name="4" type="text">
+        <input type="submit">
+    </form>
+</body>
+</html>
